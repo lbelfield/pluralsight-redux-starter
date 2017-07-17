8d5628b552ff024d1011b420be89e341c12428e2
Luke Belfield New <LBelfield@luke-belfields-computer-5.local>
2017-07-17 16:08:16 +0100

    Courses page addition

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import CoursesPage from './components/course/CoursesPage';
 
 export default (
     // by placing app at the top, we are saying always load the app component 
@@ -11,5 +12,6 @@ export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomePage} />
         <Route path="about" component={AboutPage} />
+        <Route path="courses" component={CoursesPage} />
     </Route>
 );
