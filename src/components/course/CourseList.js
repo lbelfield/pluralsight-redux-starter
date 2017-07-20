9d75dbbe3f0bdf5df7cad77917a60f16d03dd4c8
Luke Belfield New <LBelfield@luke-belfields-computer-5.local>
2017-07-20 13:40:32 +0100

    extra comments

--- src/components/course/CourseList.js
+++ src/components/course/CourseList.js
@@ -2,6 +2,14 @@ import React, {PropTypes, Component} from 'react';
 import CourseListRow from './CourseListRow';
 
 const CourseList = ({courses}) => {
+    // we have a table with headers and a body. Note the CourseListRow will contain the <td>
+    // from our Container Component, we passed down courses (line 4) into props. 
+    // this array is explicitly defined in our propTypes. 
+    // .map() accepts a function(course){} and is basically a foreach loop.
+    // courses is your array, course is your object in the array.
+    // foreach course in courses, it returns the <CourseListRow /> element
+    // with course.id is the index of the foreach loop (course.id has to be unique, hence it is the key)
+    // we then pass a course object down to the CourseListRow Component.
     return(
         <table className="table">
             <thead>
