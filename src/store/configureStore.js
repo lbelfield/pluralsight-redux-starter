6ffc1f1729211bc22340524f8e7b53a362f0548e
Luke Belfield <luke.belfield@thetrainline.com>
2017-12-19 14:42:37 +0000

    Redux ChromeDevTools Plugin

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,16 +4,19 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
-    debugger;
-    // createStore takes in three parameters
-        // 1 is our rootReducer (reducers/index.js) that combines all of our reducers
-        // 2 is the initial state, which was passed in
-        // 3 is applyMiddleware got from 'redux'. It provides a third party extension point between dispatching an action and reducers.
-            // need to add thunk, which handles asynchronous calls. Note this is used in ActionCreators
-            // reduxImmutableStateInvariant detects whether there is mutable code (which would be bad)
-    return createStore(
-        rootReducer, 
-        initialState,
+  // createStore takes in three parameters
+      // 1 is our rootReducer (reducers/index.js) that combines all of our reducers
+      // 2 is the initial state, which was passed in
+      // 3 is applyMiddleware got from 'redux'. It provides a third party extension point between dispatching an action and reducers.
+          // need to add thunk, which handles asynchronous calls. Note this is used in ActionCreators
+          // reduxImmutableStateInvariant detects whether there is mutable code (which would be bad)
+
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  return createStore(
+      rootReducer,
+      initialState,
+      composeEnhancers(
         applyMiddleware(thunk, reduxImmutableStateInvariant())
-        );
+      ));
 }
